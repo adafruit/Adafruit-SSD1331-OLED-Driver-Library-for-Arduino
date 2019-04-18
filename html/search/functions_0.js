af14c7f695d7182cb610fdb9732a27f589af4c78
Travis CI <travis@travis-ci.org>
2019-04-18 15:33:56 +0000

    Deploy code docs to GitHub Pages Travis build: 10
    Commit: c07f8ffed6b56db2fe5abfbf4c822eb052ac3264

--- html/search/functions_0.js
+++ html/search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['adafruit_5fssd1331',['Adafruit_SSD1331',['../class_adafruit___s_s_d1331.html#ae8bad7cfe4485c9b442008be60023660',1,'Adafruit_SSD1331::Adafruit_SSD1331(uint8_t _CS, uint8_t _DC, uint8_t _MOSI, uint8_t _SCLK, uint8_t _RST)'],['../class_adafruit___s_s_d1331.html#af6f6d5ef85fd4d37cfb43846c3dc88ea',1,'Adafruit_SSD1331::Adafruit_SSD1331(uint8_t _CS, uint8_t _DC, uint8_t _RST)']]]
+  ['adafruit_5fssd1331',['Adafruit_SSD1331',['../class_adafruit___s_s_d1331.html#a741732cd4194fd5b61c284359ccac204',1,'Adafruit_SSD1331::Adafruit_SSD1331(int8_t cs, int8_t dc, int8_t mosi, int8_t sclk, int8_t rst)'],['../class_adafruit___s_s_d1331.html#af6eefebf610c2fcc24fd408c74fe9cbc',1,'Adafruit_SSD1331::Adafruit_SSD1331(int8_t cs, int8_t dc, int8_t rst)'],['../class_adafruit___s_s_d1331.html#a4836cd449bca9612b56c3031d2c5bf2b',1,'Adafruit_SSD1331::Adafruit_SSD1331(SPIClass *spi, int8_t cs, int8_t dc, int8_t rst=-1)']]]
 ];
